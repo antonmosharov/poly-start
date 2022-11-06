3fe00d90bf4ef9ddf8f62874e5e7ea05dfae2779
Anton Mosharov <anton.mosharov@gmail.com>
2022-11-06 22:56:53 +0300

    Hello world

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
           <Image centered size='tiny' src={iconDevx} style={{marginBottom:"2em"}}/>
           <p>Create React Ethereum DApp on Polygon</p>
           <p>Build your first app with Starter Kits</p>
+          <p>💜 Hello world 💜</p>
         </Container>
       </Content>
       <Footer/>
